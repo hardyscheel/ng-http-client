fa0a16e757e4be5f988d1d7fd68644d8198a69df
Hardy Scheel <hardy.scheel@gmail.com>
2025-02-24 02:15:08 +0100

    Update githubuser.component.ts

--- client/src/app/component/githubuser/githubuser.component.ts
+++ client/src/app/component/githubuser/githubuser.component.ts
@@ -11,7 +11,7 @@ import { GitHubUser } from '../../model/githubuser.model';
 })
 export class GitHubUserComponent implements OnInit {
 
-  userName: string = "HardyScheel";
+  userName: string = "hascheel";
   gitHubUser: GitHubUser;
 
   loading: boolean = false;
